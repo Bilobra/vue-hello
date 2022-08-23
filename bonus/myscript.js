36d85ac515fa9b1605e3863cd1a9ea648e9fca69
Bilobra <bra.carrozzo@gmail.com>
2022-08-23 14:43:16 +0200

    add img bonus

--- bonus/myscript.js
+++ bonus/myscript.js
@@ -1,6 +1,8 @@
 const app = new Vue({
     el : '#root',
     data : {
-        message : 'Humuhumunukunukuapuaa'
+        message : 'Humuhumunukunukuapuaa',
+        img : 'https://molokinicrater.com/wp-content/uploads/2017/12/humuhumu-fish.jpg',
+        imgAlt : 'hawaii-state-fish'
     },
 })
